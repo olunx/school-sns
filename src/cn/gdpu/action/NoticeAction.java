432f6b76299a707a9f68a9142b378fe57d0ba540
fatkuns <fatkuns@gmail.com>
2010-03-27 23:43:27 +0800

    ??????

--- src/cn/gdpu/action/NoticeAction.java
+++ src/cn/gdpu/action/NoticeAction.java
@@ -1,11 +1,23 @@
 package cn.gdpu.action;
 
+import java.util.Date;
+
+import cn.gdpu.service.*;
+import cn.gdpu.vo.*;
+
 @SuppressWarnings("serial")
 public class NoticeAction extends BaseAction {
-
+	private NoticeService<Notice, Integer> noticeService;
+	private String title;
+	private String content;
 	@Override
 	public String add() {
-		// TODO Auto-generated method stub
+		Notice notice = new Notice();
+		notice.setTitle(title.trim());
+		notice.setContent(content);
+		notice.setAuthor((Student) this.getSession().get("student"));
+		notice.setTime(new Date());
+		noticeService.addEntity(notice);
 		return super.add();
 	}
 
@@ -43,6 +55,30 @@ public class NoticeAction extends BaseAction {
 	public String modify() {
 		// TODO Auto-generated method stub
 		return super.modify();
+	}
+
+	public NoticeService<Notice, Integer> getNoticeService() {
+		return noticeService;
+	}
+
+	public void setNoticeService(NoticeService<Notice, Integer> noticeService) {
+		this.noticeService = noticeService;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
 	}
 
 }
