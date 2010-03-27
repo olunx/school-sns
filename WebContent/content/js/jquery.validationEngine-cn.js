829355762ed5a1530fe30b61d85537f8ec73d8ea
me <me@olunx.com>
2010-03-27 13:41:06 +0800

    ?????JQ????????JQ??????????share???

--- WebContent/content/js/jquery.validationEngine-cn.js
+++ WebContent/content/js/jquery.validationEngine-cn.js
@@ -1,4 +1,4 @@
-﻿
+
 
 (function($) {
 	$.fn.validationEngineLanguage = function() {};
@@ -6,53 +6,57 @@
 		newLang: function() {
 			$.validationEngineLanguage.allRules = 	{"required":{    			// Add your regex rules here, you can take telephone as an example
 						"regex":"none",
-						"alertText":"* 不能为空",
-						"alertTextCheckboxMultiple":"* 请选择一个项目",
-						"alertTextCheckboxe":"* 必须选中一个"},
+						"alertText":"* 此栏为必填项",
+						"alertTextCheckboxMultiple":"* 请选择一个选项",
+						"alertTextCheckboxe":"* 此选择框为必选"},
 					"length":{
 						"regex":"none",
-						"alertText":"*必须在 ",
-						"alertText2":" 个和 ",
-						"alertText3": " 个字符之间"},
+						"alertText":"* 请输入 ",
+						"alertText2":" 至 ",
+						"alertText3": " 位数字/字符"},
 					"maxCheckbox":{
 						"regex":"none",
-						"alertText":"* 超出允许选择个数"},	
+						"alertText":"* 复选框超过最大可选数"},	
 					"minCheckbox":{
 						"regex":"none",
-						"alertText":"* 请选择 ",
+						"alertText":"* 请至少选择 ",
 						"alertText2":" 项"},	
 					"confirm":{
 						"regex":"none",
-						"alertText":"* 两次输入的不一样"},		
+						"alertText":"* 您的输入有误，请重新输入"},		
 					"telephone":{
-						"regex":"/^[0-9\-\(\)\ ]+$/",
-						"alertText":"* 不正确的手机号码"},	
+						//"regex":"/^[0-9\-\(\)\ ]+$/",
+						"regex":"\d{3}-\d{8}|\d{4}-\d{7}|\d{8}|\d{7}",
+						"alertText":"* 您输入的电话号码格式有误"},	
+					"mobilenumber":{
+						"regex":"^1[358]\d{9}$|^1060[1-9]\d{1,2}\d{7,8}$",
+						"alertText":"* 您输入的手机号码格式有误"},
 					"email":{
 						"regex":"/^[a-zA-Z0-9_\.\-]+\@([a-zA-Z0-9\-]+\.)+[a-zA-Z0-9]{2,4}$/",
-						"alertText":"* 错误的邮箱地址"},	
+						"alertText":"* 您输入的电子邮件地址格式有误"},	
 					"date":{
                          "regex":"/^[0-9]{4}\-\[0-9]{1,2}\-\[0-9]{1,2}$/",
-                         "alertText":"* 错误的日期格式, 必须为 YYYY-MM-DD "},
+                         "alertText":"* 您输入的时间格式有误，时间格式为：YYYY-MM-DD"},
 					"onlyNumber":{
 						"regex":"/^[0-9\ ]+$/",
-						"alertText":"* 只能输入数字"},	
+						"alertText":"* 请输入数字"},	
 					"noSpecialCaracters":{
 						"regex":"/^[0-9a-zA-Z]+$/",
-						"alertText":"* 只能输入数字和字母"},	
+						"alertText":"* 请去掉特殊字符，重新输入"},	
 					"ajaxUser":{
 						"file":"validateUser.php",
 						"extraData":"name=eric",
-						"alertTextOk":"* 当前用户可用",	
-						"alertTextLoad":"* 正在加载数据",
-						"alertText":"* 这个用户已经被注册了"},	
+						"alertTextOk":"* 该用户名可用",	
+						"alertTextLoad":"* 验证中，请稍后……",
+						"alertText":"* 该用户名已存在"},	
 					"ajaxName":{
 						"file":"validateUser.php",
-						"alertText":"* 这个名称已经被使用了",
-						"alertTextOk":"* 这个名称可以使用",	
-						"alertTextLoad":"* 正在加载数据"},		
+						"alertText":"* 该用户名已存在",
+						"alertTextOk":"* 该用户名可用",	
+						"alertTextLoad":"* 验证中，请稍后……"},		
 					"onlyLetter":{
 						"regex":"/^[a-zA-Z\ \']+$/",
-						"alertText":"* 只能输入字母"}
+						"alertText":"* 请输入英文字符"}
 					}	
 		}
 	}
