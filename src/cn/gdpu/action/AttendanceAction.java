59e28010e478f619aae5501cbcb71048af5675f5
fatkuns <fatkuns@gmail.com>
2010-04-03 22:47:04 +0800

    ??????

--- src/cn/gdpu/action/AttendanceAction.java
+++ src/cn/gdpu/action/AttendanceAction.java
@@ -1,33 +1,75 @@
 package cn.gdpu.action;
 
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import cn.gdpu.service.AttendanceService;
 import cn.gdpu.service.CourseService;
 import cn.gdpu.service.StudentService;
+import cn.gdpu.util.PageBean;
+import cn.gdpu.vo.Attendance;
 import cn.gdpu.vo.Course;
 import cn.gdpu.vo.Student;
-
 
 @SuppressWarnings("serial")
 public class AttendanceAction extends BaseAction {
 	private StudentService<Student, Integer> studentService;
 	private CourseService<Course, Integer> courseService;
+	private AttendanceService<Attendance, Integer> attendanceService;
+	private String week;
+	private String day;
+	private String courseIds;
+	private String students;// 接收传来的学生学号字符串
+	private int id;
+	private Integer[] ids;//批量删除ID
+	private int cmd;
 
 	@Override
 	public String add() {
-		// TODO Auto-generated method stub
+		Attendance attendance = new Attendance();
+		attendance.setWeek(week);
+		attendance.setDay(day);
+
+		// 获取所有选择的课程对象
+		String[] cids = courseIds.split("\\,");
+		Set<Course> courses = new HashSet<Course>();
+		for (int i = 0; i < cids.length; i++) {
+			if (!cids[i].equals("")) {
+				courses.add(courseService.getEntity(Course.class, Integer.parseInt(cids[i])));
+			}
+		}
+
+		// 获取所有选择的学生对象
+		String[] stuSnos = students.split("\\,");
+		Set<Student> stus = new HashSet<Student>();
+		for (int i = 0; i < stuSnos.length; i++) {
+			if (!stuSnos[i].equals(""))
+				stus.add(studentService.getStudentByNo(stuSnos[i]));
+		}
+
+		attendance.setCourse(courses);
+		attendance.setStudents(stus);
+		attendance.setClerk((Student) this.getSession().get("student"));
+		attendance.setTime(new Date());
+		attendanceService.addEntity(attendance);
 		return super.add();
 	}
 
 	@Override
 	public String delete() {
-		// TODO Auto-generated method stub
+		attendanceService.deleteEntity(Attendance.class, id);
 		return super.delete();
 	}
 
 	@Override
 	public String deleteMany() {
-		// TODO Auto-generated method stub
+		switch (cmd) {
+		case 1:
+		attendanceService.deleteManyEntity(Attendance.class, ids);
+		break;
+		};
 		return super.deleteMany();
 	}
 
@@ -35,10 +77,10 @@ public class AttendanceAction extends BaseAction {
 	public String goAdd() {
 		List<Student> students = studentService.getAllEntity(Student.class);
 		List<Course> courses = courseService.getAllEntity(Course.class);
-		
+
 		getRequest().put("students", students);
-		getRequest().put("courses",courses);
-		
+		getRequest().put("courses", courses);
+
 		return super.goAdd();
 	}
 
@@ -50,7 +92,10 @@ public class AttendanceAction extends BaseAction {
 
 	@Override
 	public String list() {
-		// TODO Auto-generated method stub
+		PageBean pageBean = attendanceService.queryForPage(Attendance.class, 15, 1);
+		this.getRequest().put("pageBean", pageBean);
+		if (pageBean.getList().isEmpty())
+			pageBean.setList(null);
 		return super.list();
 	}
 
@@ -58,6 +103,86 @@ public class AttendanceAction extends BaseAction {
 	public String modify() {
 		// TODO Auto-generated method stub
 		return super.modify();
+	}
+
+	public StudentService<Student, Integer> getStudentService() {
+		return studentService;
+	}
+
+	public void setStudentService(StudentService<Student, Integer> studentService) {
+		this.studentService = studentService;
+	}
+
+	public CourseService<Course, Integer> getCourseService() {
+		return courseService;
+	}
+
+	public void setCourseService(CourseService<Course, Integer> courseService) {
+		this.courseService = courseService;
+	}
+
+	public String getWeek() {
+		return week;
+	}
+
+	public void setWeek(String week) {
+		this.week = week;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public String getCourseIds() {
+		return courseIds;
+	}
+
+	public void setCourseIds(String courseIds) {
+		this.courseIds = courseIds;
+	}
+
+	public String getStudents() {
+		return students;
+	}
+
+	public void setStudents(String students) {
+		this.students = students;
+	}
+
+	public AttendanceService<Attendance, Integer> getAttendanceService() {
+		return attendanceService;
+	}
+
+	public void setAttendanceService(AttendanceService<Attendance, Integer> attendanceService) {
+		this.attendanceService = attendanceService;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Integer[] getIds() {
+		return ids;
+	}
+
+	public void setIds(Integer[] ids) {
+		this.ids = ids;
+	}
+
+	public int getCmd() {
+		return cmd;
+	}
+
+	public void setCmd(int cmd) {
+		this.cmd = cmd;
 	}
 
 }
