30cb83a1d9d09506bc79a1b7a27195c9927b7211
fatkuns <fatkuns@gmail.com>
2010-04-10 15:21:18 +0800

    ??????????????

--- WebContent/WEB-INF/header.jsp
+++ WebContent/WEB-INF/header.jsp
@@ -7,103 +7,37 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>School Social</title>
-<link type="text/css" rel="stylesheet" href="<%=path%>/content/images/reset.css" />
+<!-- <link type="text/css" rel="stylesheet" href="<%=path%>/content/images/reset.css" />-->
 <link type="text/css" rel="stylesheet" href="<%=path%>/content/images/layout.css" />
-<link type="text/css" rel="stylesheet" href="<%=path%>/content/images/header.css" />
-<link type="text/css" rel="stylesheet" href="<%=path%>/content/images/content.css" />
-<link type="text/css" rel="stylesheet" href="<%=path%>/content/images/footer.css" />
 <!-- JQuery库 -->
 <script type="text/javascript" src="<%=path%>/content/js/jquery-1.4.2.min.js"></script>
 <!-- colorbox -->
 <link type="text/css" rel="stylesheet" href="<%=path%>/content/jq-colorbox/colorbox.css" />
 <script type="text/javascript" src="<%=path%>/content/jq-colorbox/jquery.colorbox-min.js"></script>
 
-<script type="text/javascript">
-	$(document).ready(function() {
+<script type="text/javascript" src="<%=path%>/content/js/jquery.corner.js"></script>
 
-		$("a[rel='dialog']").colorbox();
-		
-		//注册事件
-			$("a[target='content']").click(function() {
-				var href = $(this).attr('href');
-				loadContent(href);
-				return false;
-			});
-
-		});
-
-	//注册二级事件
-	function ajax() {
-		$("a[rel='dialog']").colorbox();
-		$("#content a[target='content']").click(function() {
-			var href = $(this).attr('href');
-			loadContent(href);
-			return false;
-		});
-	};
-
-	//加载数据
-	function loadContent(href) {
-		var content = $('#content');
-		content.fadeOut('fast', function() {
-			onLoading();//打开loading
-			content.load(href, function() {
-				offLoading();//关闭loading
-				content.fadeIn('normal', ajax);
-			});
-		});
-	}
-
-	//打开loading
-	function onLoading() {
-		$('#main').append('<span id="loading"><img src="<%=path%>/content/jq-colorbox/images/loading.gif" />加载中...</span>');
-		$('#loading').fadeIn('fast');
-	}
-	//关闭loading
-	function offLoading() {
-		$('#loading').remove();
-	}
-	
-	//提交表单数据
-	function post(obj) {
-		var content = $('#content');
-		var urlStr = $(obj).attr('action');
-		var dataStr = decodeURIComponent($(obj).serialize());
-		content.fadeOut('fast', function() {
-			onLoading();//打开loading
-			$.ajax( {
-				url : urlStr,
-				data : dataStr,
-				type : 'POST',
-				success : function(result) {
-					content.html(result);
-					offLoading();//关闭loading
-					content.fadeIn('normal', ajax);
-				}
-			});
-		});
-	}
-</script>
+<!-- 常用库，确保这段代码在最下方 -->
+<script type="text/javascript" src="<%=path%>/content/js/common.js"></script>
 </head>
-<body background="<%=path%>/content/images/bg.jpg">
-<div class="header_k">
-<div id="header">
-<div class="headerwarp">
-<div class="logo"><a href="">LOGO</a></div>
-<div class="menuk">
-<ul class="menu">
-	<li><a href="<%=path%>/login.jsp">首页</a></li>
-	<li><a href="#">学校</a></li>
-	<li><a href="#">群组</a></li>
-	<li><a href="#">消息</a></li>
-	<li><a href="#">博客</a></li>
-</ul>
-<div class="nav_account"><span class="loginName">欢迎</span>
- <a rel="dialog" href="<%=path%>/mail/listMyReceMail">小纸条</a>
- <a rel="dialog" href="<%=path%>/login/goLogin">登录</a> | <a href="">注册</a></div>
-<div id="dialog" /></div>
+<body>
+
+  <div id="header">
+      <div class="logo"><a href="">LOGO</a></div>
+      <div class="menuk corner">
+        <ul class="menu">
+          <li><a href="<%=path%>/login.jsp">首页</a></li>
+          <li><a href="#">学校</a></li>
+          <li><a href="#">群组</a></li>
+          <li><a href="#">消息</a></li>
+          <li><a href="#">博客</a></li>
+        </ul>
+        <div class="nav_account"><span class="loginName">欢迎</span> <a rel="dialog" href="<%=path%>/mail/listMyReceMail">小纸条</a> <a
+	rel="dialog" href="<%=path%>/login/goLogin">登录</a> | <a href="">注册</a>
+		</div>
+        <div id="dialog"></div>
+    </div>
+
 </div>
-</div>
-</div>
-</div>
+
 <div id="container">
--- src/cn/gdpu/chat/MyChat.java
+++ src/cn/gdpu/chat/MyChat.java
@@ -16,33 +16,44 @@ import org.directwebremoting.ScriptSessions;
 import org.directwebremoting.WebContextFactory;
 import org.directwebremoting.proxy.dwr.Util;
 
+
+import cn.gdpu.util.Log;
+import cn.gdpu.vo.Student;
+
 public class MyChat {
-	Map<String, ScriptSession> sm = new HashMap<String, ScriptSession>();
-	LinkedList<User> userList = UserList.getInstance().getUserList();
+	Map<String, ScriptSession> sm = new HashMap<String, ScriptSession>(); //httpSession与ScriptSession对应表
+	LinkedList<User> userList = UserList.getInstance().getUserList(); //用户表
 
 	@SuppressWarnings( { "deprecation" })
-	public String updateUsersList(String username, HttpServletRequest request) {
+	public String updateUsersList(HttpServletRequest request) {
 		User user = null;
-		if (!username.equals("")) {
-			user = new User(username);
-			request.getSession().setAttribute("user", user);
+
+		//取得学生
+		Student stu = (Student) request.getSession().getAttribute("student");
+		Log.init(this.getClass()).info("登陆名："+stu.getUsername());
+		if (stu != null) {
+			if (request.getSession().getAttribute("chatuser") == null){
+				Log.init(this.getClass()).info("新增用户");
+				user = new User(stu.getUsername());
+				request.getSession().setAttribute("chatuser", user);
+				}
 		}
 		sm.put(request.getSession().getId(), WebContextFactory.get().getScriptSession());
-		// System.out.println(sm);
-		//System.out.println(user.getName());
+		Log.init(this.getClass()).info("sm："+sm);
+		
 		List<String> list = new ArrayList<String>();
 		for (User u : userList) {
 			list.add(u.getName());
 		}
 		for (Object o : sm.keySet()) {
 			ScriptSession ss = sm.get(o);
-			ss.addScript(new ScriptBuffer().appendCall("updateOnlineUser", userList));
+			ss.addScript(new ScriptBuffer().appendCall("receiveOnlineUser", userList));
 			Util util = new Util(ss);
 			util.removeAllOptions("sendto");
 			util.addOptions("sendto", new String[] { "所有人" });
 			util.addOptions("sendto", userList, "userid", "name");
 		}
-		return null;
+		return request.getSession().getId();
 	};
 
 	public void sayHi() {
@@ -53,35 +64,50 @@ public class MyChat {
 		});
 	}
 
-	public void sayTo(String sendTo, String msg, HttpServletRequest request) {
-		User user = (User) request.getSession().getAttribute("user");
-		String username = user.getName();
-		String userid = user.getUserid();
+	public void sayTo(String toId, String msg, HttpServletRequest request) {
+		Log.init(this.getClass()).info("msg："+msg);
+		User user = (User) request.getSession().getAttribute("chatuser");
+		Log.init(this.getClass()).info("user："+user);
+		String fromUsername = user.getName();
+		String fromId = user.getUserid();
 		String date = new SimpleDateFormat("hh:mm:ss").format(new Date());
-		final String sendStr = username + "说:" + msg + " " + date;
-		String sendToUsername = sendTo;
+		final String sendStr = fromUsername + "说:" + msg + " " + date;
+		String toUsername = toId;
+		Log.init(this.getClass()).info("userList："+userList);
 		for (User u : userList) {
-			if (u.getUserid().equals(sendTo)) {
-				sendToUsername = u.getName();
+			if (u.getUserid().equals(toId)) {
+				toUsername = u.getName();
 				break;
 			}
 		}
-		for (String o : sm.keySet()) {
-			ScriptSession ss = sm.get(o);
-			if (sendTo.equals("所有人")) {
-				ss.addScript(new ScriptBuffer().appendCall("show", sendStr));
-			} else {
-				if (sendTo.equals(o))
-					ss.addScript(new ScriptBuffer().appendCall("show", username + "对你说:" + msg + " " + date));
-				if (userid.equals(o))
-					ss.addScript(new ScriptBuffer().appendCall("show", "你对" + sendToUsername + "说:" + msg + " " + date));
-			}
-		}
-		// Browser.withAllSessions(new Runnable() {
-		// public void run() {
-		// ScriptSessions.addFunctionCall("show", sendStr);
-		// }
-		// });
+		
+		//信息类
+		final Message message = new Message();
+		message.setText(msg);
+		message.setFrom(fromUsername);
+		message.setFromid(fromId);
+		message.setTo(toUsername);
+		message.setToid(toId);
+		message.setTime(date);
+		
+//		Log.init(this.getClass()).info("遍历sm："+sm);
+//		for (String o : sm.keySet()) {
+//			ScriptSession ss = sm.get(o);
+//			if (toId.equals("所有人")) {
+//				ss.addScript(new ScriptBuffer().appendCall("show", sendStr));
+//			} else {
+//				if (toId.equals(o))
+//					ss.addScript(new ScriptBuffer().appendCall("show", fromUsername + "对你说:" + msg + " " + date));
+//				if (fromId.equals(o))
+//					ss.addScript(new ScriptBuffer().appendCall("show", "你对" + toUsername + "说:" + msg + " " + date));
+//			}
+//		}
+		
+		 Browser.withAllSessions(new Runnable() {
+		 public void run() {
+		 ScriptSessions.addFunctionCall("receiveMessage", message);
+		 }
+		 });
 	}
 
 }
