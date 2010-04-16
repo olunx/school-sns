319283cb5ee45765f7120b9b8bd95693dc84d7c5
me <me@olunx.com>
2010-04-16 17:07:17 +0800

    ?????

--- src/cn/gdpu/dao/impl/ADaoImpl.java
+++
@@ -1,8 +0,0 @@
-package cn.gdpu.dao.impl;
-
-
-import cn.gdpu.vo.A;
-
-public class ADaoImpl extends BaseDaoImpl<A, Integer> implements cn.gdpu.dao.ADao<A, Integer> {
-
-}
--- src/cn/gdpu/service/impl/AServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package cn.gdpu.service.impl;
-
-import cn.gdpu.dao.impl.ADaoImpl;
-import cn.gdpu.service.AService;
-import cn.gdpu.vo.A;
-
-public class AServiceImpl extends BaseServiceImpl<A, Integer, ADaoImpl> implements AService<A, Integer> {
-
-}
